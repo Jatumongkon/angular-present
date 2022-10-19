274afbbad10c5cae5374b9106bf73742fe740fd5
jatumongkhon Saengphum <jatumongkhon.s@betimes.biz>
2022-10-19 11:57:49 +0700

    add dx-button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { DxButtonModule } from 'devextreme-angular/ui/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +17,8 @@ import { SecondRouteComponent } from './second-route/second-route.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DxButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
